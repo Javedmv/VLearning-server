0ec56aeb6c2d7efa8182fd7fc619887e7ba3562d
Javedmv <javedmv777@gmail.com>
2024-12-09 11:42:11 +0530

    fix: verifyToken of admin&user

--- auth-service/src/_lib/jwt/verifyAdmin.ts
+++ auth-service/src/_lib/jwt/verifyAdmin.ts
@@ -1,30 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import { ErrorResponse } from "../common/error";
-import jwt from "jsonwebtoken";
 
 
-export const verifyAdmin = (req: Request, res: Response, next: NextFunction) => {
+export const verifyAdmin = async(req:Request, res:Response, next: NextFunction) => {
     try {
-      const token = req.cookies.access_token || req.headers.authorization?.split(" ")[1];
-      if (!token) {
-        return next(ErrorResponse.unauthorized("Token not found"));
-      }
-  
-      const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET!) as {
-        _id: string;
-        email: string;
-        role: string;
-      };
-  
-      if (decoded.role !== "admin") {
-        return next(ErrorResponse.unauthorized("Role mismatch"));
-      }
-  
-      req.user = decoded;
-  
-      next();
+        if (!req.user) {
+            return next(ErrorResponse.unauthorized("Token not found, verify admin"));
+          }
+      
+          if (req.user.role !== "admin") {
+            return next(ErrorResponse.unauthorized("Role mismatch, verify admin"));
+          }
+      
+          next();
     } catch (error) {
-      console.error("Error in verifying admin:", error);
-      return next(ErrorResponse.unauthorized("Invalid or expired token"));
+        console.error("Error in JWT verify user:",error)
+        next(error)
     }
-  };
+}
--- auth-service/src/_lib/jwt/verifyUser.ts
+++ auth-service/src/_lib/jwt/verifyUser.ts
@@ -1,23 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import { ErrorResponse } from "../common/error";
-import { findById } from "../../infrastructure/database/mongoDB/repositories";
 
 
 export const verifyUser = async(req:Request, res:Response, next: NextFunction) => {
     try {
-        if(!req.user){
-            return next(ErrorResponse.unauthorized("Token not found,verify student"))
+        if (!req.user) {
+            return next(ErrorResponse.unauthorized("Token not found"));
         }
-    
-        const user = await findById(req.user._id);
-        if (!user) {
-            return next(ErrorResponse.unauthorized("user(student or instructor) not found"));
+        const validRoles = ["student", "instructor"];
+        if (!validRoles.includes(req.user.role)) {
+            return next(ErrorResponse.unauthorized("Role mismatch"));
         }
-    
-        if (user.role !== "student" && user.role !== "instructor") {
-            return next(ErrorResponse.unauthorized("Role mismatch,verify student"));
-        }
-        
         next()
     } catch (error) {
         console.error("Error in JWT verify user:",error)
