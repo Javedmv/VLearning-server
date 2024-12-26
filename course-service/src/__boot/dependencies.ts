a5909ec2876aade7cd81ae5ea827c2e514ffe7a9
Javedmv <javedmv777@gmail.com>
2024-12-26 18:09:33 +0530

    created course-service

---
+++ course-service/src/__boot/dependencies.ts
@@ -0,0 +1,8 @@
+// import * as repositories from "../infrastructure/databases/mongoDB/repositories";
+// import * as useCases from "../application/useCases";
+
+
+// export const dependencies: IDependencies = {
+//     repositories,
+//     useCases
+// }
---
+++ course-service/src/infrastructure/routes/index.ts
@@ -0,0 +1,9 @@
+import {IDependencies} from "../../application/interfaces/IDependencies"
+import { Router } from "express"
+
+export const courseRoutes = (dependencies:IDependencies) => {
+
+    const router = Router();
+
+    return router;
+}
