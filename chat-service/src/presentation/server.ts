4a079edeb3e32f9e4a7db8f4fb4f9e8b3e692282
Javedmv <javedmv777@gmail.com>
2025-02-21 00:25:39 +0530

    service setup done

--- chat-service/src/presentation/server.ts
+++ chat-service/src/presentation/server.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 
 import { PORT } from "../__boot/config";
+import { routes } from "../infrastructure/routes";
+import { dependencies } from "../__boot/dependencies";
 
 dotenv.config();
 
@@ -12,6 +14,8 @@ const app: Application = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
+
+app.use(routes(dependencies))
 
 app.listen(PORT,() => {
     console.log(`Chat server running on port: http://localhost${PORT}`);
