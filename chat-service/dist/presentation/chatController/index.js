28bf4eb57ae8840b5ca476adb5d33341cb559042
Javedmv <javedmv777@gmail.com>
2025-02-20 20:35:56 +0530

    Removed node_modules and dist from Git tracking in chat-service

--- chat-service/dist/presentation/chatController/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chatControllers = void 0;
-const chatControllers = (dependencies) => {
-    return {};
-};
-exports.chatControllers = chatControllers;
